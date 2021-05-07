d762eb9b2bb1d33c91c28ad000d1ec20b089fe3e
nelson <memoryman51@hotmail.com>
2021-05-07 09:59:38 +0200

    add plugin config file, move require statements to packer

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+require('pluginconfig')
 require('plugins')
 require('lv-globals')
 vim.cmd('luafile ~/.config/nvim/lv-settings.lua')
@@ -5,19 +6,19 @@ require('lv-utils')
 require('settings')
 require('lv-autocommands')
 require('keymappings')
-require('lv-nvimtree') -- This plugin must be required somewhere before colorscheme.  Placing it after will break navigation keymappings
+--require('lv-nvimtree') -- This plugin must be required somewhere before colorscheme.  Placing it after will break navigation keymappings
 require('colorscheme') -- This plugin must be required somewhere after nvimtree. Placing it before will break navigation keymappings 
 require('lv-galaxyline')
 require('lv-comment')
 require('lv-compe')
 require('lv-barbar')
-require('lv-dashboard')
+-- require('lv-dashboard')
 require('lv-telescope')
 require('lv-gitsigns')
 require('lv-treesitter')
 require('lv-autopairs')
 require('lv-rnvimr')
-require('lv-which-key')
+-- require('lv-which-key')
 require('colorizer')
 
 -- TODO is there a way to do this without vimscript
