3266d92eb1e155cfa812d268f57a29be5e1591a1
rebuilt <memoryman51@hotmail.com>
2021-05-18 00:07:09 +0200

    formatting

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -1,120 +1,120 @@
 local config = {}
 
 function config.nvim_lsp()
-  require('modules.completion.lspconfig')
+	require('modules.completion.lspconfig')
 end
 
 function config.nvim_compe()
-  require'compe'.setup {
-    enabled = true;
-    debug = false;
-    min_length = 1;
-    preselect = 'always';
-    allow_prefix_unmatch = false;
-    source = {
-      path = true;
-      buffer = true;
-      calc = true;
-      vsnip = true;
-      nvim_lsp = true;
-      nvim_lua = true;
-      spell = true;
-      tags = true;
-      snippets_nvim = false;
-    };
-  }
+	require'compe'.setup {
+		enabled = true;
+		debug = false;
+		min_length = 1;
+		preselect = 'always';
+		allow_prefix_unmatch = false;
+		source = {
+			path = true;
+			buffer = true;
+			calc = true;
+			vsnip = true;
+			nvim_lsp = true;
+			nvim_lua = true;
+			spell = true;
+			tags = true;
+			snippets_nvim = false;
+		};
+	}
 end
 
 function config.auto_pairs()
-  local remap = vim.api.nvim_set_keymap
-  local npairs = require('nvim-autopairs')
-  npairs.setup()
-  -- skip it, if you use another global object
-  _G.MUtils= {}
+	local remap = vim.api.nvim_set_keymap
+	local npairs = require('nvim-autopairs')
+	npairs.setup()
+	-- skip it, if you use another global object
+	_G.MUtils= {}
 
-  vim.g.completion_confirm_key = ""
-  MUtils.completion_confirm=function()
-    if vim.fn.pumvisible() ~= 0  then
-      if vim.fn.complete_info()["selected"] ~= -1 then
-        return vim.fn["compe#confirm"](npairs.esc("<cr>"))
-      else
-        return npairs.esc("<cr>")
-      end
-    else
-      return npairs.autopairs_cr()
-    end
-  end
+	vim.g.completion_confirm_key = ""
+	MUtils.completion_confirm=function()
+		if vim.fn.pumvisible() ~= 0  then
+			if vim.fn.complete_info()["selected"] ~= -1 then
+				return vim.fn["compe#confirm"](npairs.esc("<cr>"))
+			else
+				return npairs.esc("<cr>")
+			end
+		else
+			return npairs.autopairs_cr()
+		end
+	end
 
 
-  remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
+	remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
 
-  local endwise = require('nvim-autopairs.ts-rule').endwise
+	local endwise = require('nvim-autopairs.ts-rule').endwise
 
-  npairs.add_rules({
-    endwise('def', 'end',nil,nil),
-    endwise('if', 'end',nil,nil)
-  })
+	npairs.add_rules({
+		endwise('def', 'end',nil,nil),
+		endwise('if', 'end',nil,nil)
+	})
 
 end
 
 function config.vim_vsnip()
-  vim.g.vsnip_snippet_dir = os.getenv('HOME') .. '/.config/nvim/snippets'
+	vim.g.vsnip_snippet_dir = os.getenv('HOME') .. '/.config/nvim/snippets'
 end
 
 function config.telescope()
-  if not packer_plugins['plenary.nvim'].loaded then
-    vim.cmd [[packadd plenary.nvim]]
-    vim.cmd [[packadd popup.nvim]]
-    vim.cmd [[packadd telescope-fzy-native.nvim]]
-  end
-  require('telescope').setup {
-    defaults = {
-      prompt_prefix = '🔭 ',
-      prompt_position = 'top',
-      selection_caret = " ",
-      sorting_strategy = 'ascending',
-      results_width = 0.6,
-      file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-      grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-      qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-    },
-    extensions = {
-      fzy_native = {
-        override_generic_sorter = false,
-        override_file_sorter = true,
-      }
-    }
-  }
-  require('telescope').load_extension('fzy_native')
-  require'telescope'.load_extension('dotfiles')
-  require'telescope'.load_extension('gosource')
+	if not packer_plugins['plenary.nvim'].loaded then
+		vim.cmd [[packadd plenary.nvim]]
+		vim.cmd [[packadd popup.nvim]]
+		vim.cmd [[packadd telescope-fzy-native.nvim]]
+	end
+	require('telescope').setup {
+		defaults = {
+			prompt_prefix = '🔭 ',
+			prompt_position = 'top',
+			selection_caret = " ",
+			sorting_strategy = 'ascending',
+			results_width = 0.6,
+			file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+			grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+			qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+		},
+		extensions = {
+			fzy_native = {
+				override_generic_sorter = false,
+				override_file_sorter = true,
+			}
+		}
+	}
+	require('telescope').load_extension('fzy_native')
+	require'telescope'.load_extension('dotfiles')
+	require'telescope'.load_extension('gosource')
 end
 
 function config.vim_sonictemplate()
-  vim.g.sonictemplate_postfix_key = '<C-,>'
-  vim.g.sonictemplate_vim_template_dir = os.getenv("HOME").. '/.config/nvim/template'
+	vim.g.sonictemplate_postfix_key = '<C-,>'
+	vim.g.sonictemplate_vim_template_dir = os.getenv("HOME").. '/.config/nvim/template'
 end
 
 function config.smart_input()
-  require('smartinput').setup {
-    ['go'] = { ';',':=',';' }
-  }
+	require('smartinput').setup {
+		['go'] = { ';',':=',';' }
+	}
 end
 
 function config.emmet()
-  vim.g.user_emmet_complete_tag = 0
-  vim.g.user_emmet_install_global = 0
-  vim.g.user_emmet_install_command = 0
-  vim.g.user_emmet_mode = 'i'
+	vim.g.user_emmet_complete_tag = 0
+	vim.g.user_emmet_install_global = 0
+	vim.g.user_emmet_install_command = 0
+	vim.g.user_emmet_mode = 'i'
 end
 
 function config.nvim_lspinstall()
-  require'lspinstall'.setup() -- important
+	require'lspinstall'.setup() -- important
 
-  local servers = require'lspinstall'.installed_servers()
-  for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{on_attach = enhance_attach}
-  end
+	local servers = require'lspinstall'.installed_servers()
+	for _, server in pairs(servers) do
+		require'lspconfig'[server].setup{on_attach = enhance_attach}
+	end
 end
 
 return config
