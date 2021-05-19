3a3e52b80ce6abe9913118d6423a985d3dcf81e1
rebuilt <memoryman51@hotmail.com>
2021-05-19 13:49:46 +0200

    formatting

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -1,60 +1,68 @@
 local completion = {}
-local conf = require('modules.completion.config')
+local conf = require("modules.completion.config")
 
-completion['neovim/nvim-lspconfig'] = {
-  event = 'BufReadPre',
-  config = conf.nvim_lsp,
+completion["neovim/nvim-lspconfig"] = {
+    event = "BufReadPre",
+    config = conf.nvim_lsp
 }
-completion['kabouzeid/nvim-lspinstall'] = {
-  after = 'nvim-lspconfig',
-  config = conf.nvim_lspinstall,
+completion["kabouzeid/nvim-lspinstall"] = {
+    after = "nvim-lspconfig",
+    config = conf.nvim_lspinstall
 }
-completion['windwp/nvim-autopairs'] = {
-  event = 'InsertEnter',
-  -- event = 'BufEnter',
-  config = conf.auto_pairs,
-  after = 'nvim-treesitter',
-}
-
-completion['glepnir/lspsaga.nvim'] = {
-  cmd = 'Lspsaga',
+completion["windwp/nvim-autopairs"] = {
+    event = "InsertEnter",
+    -- event = 'BufEnter',
+    config = conf.auto_pairs,
+    after = "nvim-treesitter"
 }
 
-completion['hrsh7th/nvim-compe'] = {
-  event = 'InsertEnter',
-  config = conf.nvim_compe,
+completion["glepnir/lspsaga.nvim"] = {
+    cmd = "Lspsaga"
 }
 
-completion['hrsh7th/vim-vsnip'] = {
-  after = 'nvim-compe',
-  config = conf.vim_vsnip
+completion["hrsh7th/nvim-compe"] = {
+    event = "InsertEnter",
+    config = conf.nvim_compe
 }
 
-completion['nvim-telescope/telescope.nvim'] = {
-  cmd = 'Telescope',
-  config = conf.telescope,
-  requires = {
-    {'nvim-lua/popup.nvim', opt = true},
-    {'nvim-lua/plenary.nvim',opt = true},
-    {'nvim-telescope/telescope-fzy-native.nvim',opt = true},
-  }
+completion["hrsh7th/vim-vsnip"] = {
+    after = "nvim-compe",
+    config = conf.vim_vsnip
 }
 
-completion['glepnir/smartinput.nvim'] = {
-  ft = 'go',
-  config = conf.smart_input
+-- completion["hrsh7th/vim-vsnip-integ"] = {
+--     after = "vim-vsnip"
+-- }
+--
+-- completion["rafamadriz/friendly-snippets"] = {
+--     after = "vim-vsnip"
+-- }
+
+completion["nvim-telescope/telescope.nvim"] = {
+    cmd = "Telescope",
+    config = conf.telescope,
+    requires = {
+        {"nvim-lua/popup.nvim", opt = true},
+        {"nvim-lua/plenary.nvim", opt = true},
+        {"nvim-telescope/telescope-fzy-native.nvim", opt = true}
+    }
 }
 
-completion['mattn/vim-sonictemplate'] = {
-  cmd = 'Template',
-  ft = {'go','typescript','lua','javascript','vim','rust','markdown'},
-  config = conf.vim_sonictemplate,
+completion["glepnir/smartinput.nvim"] = {
+    ft = "go",
+    config = conf.smart_input
 }
 
-completion['mattn/emmet-vim'] = {
-  event = 'InsertEnter',
-  ft = {'html','css','javascript','javascriptreact','vue','typescript','typescriptreact'},
-  config = conf.emmet,
+completion["mattn/vim-sonictemplate"] = {
+    cmd = "Template",
+    ft = {"go", "typescript", "lua", "javascript", "vim", "rust", "markdown"},
+    config = conf.vim_sonictemplate
+}
+
+completion["mattn/emmet-vim"] = {
+    event = "InsertEnter",
+    ft = {"html", "css", "javascript", "javascriptreact", "vue", "typescript", "typescriptreact"},
+    config = conf.emmet
 }
 
 return completion
