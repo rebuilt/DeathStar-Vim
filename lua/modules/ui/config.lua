c9e1d814b83101db7b8dc0a5fdc1f263965bf363
rebuilt <memoryman51@hotmail.com>
2021-05-19 15:28:37 +0200

    add bfq

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -213,4 +213,29 @@ function config.which_key()
     )
 end
 
+function config.bqf()
+    require("bqf").setup(
+        {
+            auto_enable = true,
+            preview = {
+                win_height = 12,
+                win_vheight = 12,
+                delay_syntax = 80,
+                border_chars = {"┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█"}
+            },
+            func_map = {
+                vsplit = "",
+                ptogglemode = "z,",
+                stoggleup = ""
+            },
+            filter = {
+                fzf = {
+                    action_for = {["ctrl-s"] = "split"},
+                    extra_opts = {"--bind", "ctrl-o:toggle-all", "--prompt", "> "}
+                }
+            }
+        }
+    )
+end
+
 return config
