d89ffe8e39f99b450cc427b526f61a1a5cd1aa0e
rebuilt <memoryman51@hotmail.com>
2021-06-02 21:42:47 +0200

    remove commented out code

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -26,59 +26,25 @@ local plug_map = {
     ["n|K"] = map_cr("Lspsaga hover_doc"):with_noremap():with_silent(),
     ["n|<A-t>"] = map_cu("Lspsaga open_floaterm"):with_noremap():with_silent(),
     ["t|<A-t>"] = map_cu([[<C-\><C-n>:Lspsaga close_floaterm<CR>]]):with_noremap():with_silent(),
-    --     ["n|<Leader>g"] = map_cu("Lspsaga open_floaterm lazygit"):with_noremap():with_silent(),
-    -- Plugin Telescope
-    --     ["n|<Leader>B"] = map_cu("Telescope buffers"):with_noremap():with_silent(),
-    --     ["n|<Leader>Q"] = map_cu("Telescope quickfix"):with_noremap():with_silent(),
-    --     ["n|<Leader>L"] = map_cu("Telescope loclist"):with_noremap():with_silent(),
-    --     ["n|<Leader>fb"] = map_cu("Telescope file_browser"):with_noremap():with_silent(),
-    --     ["n|<Leader>ff"] = map_cu("Telescope find_files find_command=rg,--hidden,--files"):with_noremap():with_silent(),
-    --     ["n|<Leader>fg"] = map_cu("Telescope git_files"):with_noremap():with_silent(),
-    --     ["n|<Leader>fw"] = map_cu("Telescope live_grep"):with_noremap():with_silent(),
-    --     ["n|<Leader>fc"] = map_cu("Telescope git_commits"):with_noremap():with_silent(),
-    --     ["n|<Leader>ft"] = map_cu("Telescope help_tags"):with_noremap():with_silent(),
-    --     ["n|<Leader>M"] = map_cu("Telescope marks"):with_noremap():with_silent(),
-    --     ["n|<Leader>fj"] = map_cu("Telescope jumplist"):with_noremap():with_silent(),
-    --     ["n|<Leader>fq"] = map_cu("Telescope quickfix"):with_noremap():with_silent(),
-    --     ["n|<Leader>fd"] = map_cu("Telescope dotfiles path=" .. os.getenv("HOME") .. "/.dotfiles"):with_noremap():with_silent(
 
-    --     ),
-    --     ["n|<Leader>fs"] = map_cu("Telescope gosource"):with_noremap():with_silent(),
     -- Plugin acceleratedjk
     ["n|j"] = map_cmd('v:lua.enhance_jk_move("j")'):with_silent():with_expr(),
     ["n|k"] = map_cmd('v:lua.enhance_jk_move("k")'):with_silent():with_expr(),
-    -- Plugin QuickRun
-    --     ["n|<Leader>r"] = map_cr("<cmd> lua require'internal.quickrun'.run_command()<CR>"):with_noremap():with_silent(),
-    -- Plugin Vista
-    --     ["n|<Leader>v"] = map_cu("Vista!!"):with_noremap():with_silent(),
+
     -- Plugin vim_niceblock
     ["x|I"] = map_cmd("v:lua.enhance_nice_block('I')"):with_expr(),
     ["x|gI"] = map_cmd("v:lua.enhance_nice_block('gI')"):with_expr(),
     ["x|A"] = map_cmd("v:lua.enhance_nice_block('A')"):with_expr(),
+
     -- Personal mappings
     ["n|<F7>"] = map_cr(":set relativenumber!"):with_silent(),
     ["i|jk"] = map_cmd("<ESC>"):with_silent(),
     ["i|kj"] = map_cmd("<ESC>"):with_silent(),
     ["i|jj"] = map_cmd("<ESC>"):with_silent(),
+    ["n|<ESC>"] = map_cmd(":nohls<ESC>"):with_silent(),
     ["n|<TAB>"] = map_cr(":bnext"):with_silent(),
     ["n|<S-TAB>"] = map_cr(":bprevious"):with_silent(),
-    --     ["n|<leader>go"] = map_cr("Telescope git_status"):with_silent(),
-    --     ["n|<leader>gb"] = map_cr("Telescope git_branches"):with_silent(),
-    --     ["n|<leader>gg"] = map_cr("Telescope git_commits"):with_silent(),
-    --     ["n|<leader>gG"] = map_cr("Telescope git_bcommits"):with_silent(),
     ["n|<S-x>"] = map_cr(":bd"):with_silent()
-    -- Markdown
-    --     ["n|<leader>mg"] = map_cr(":Glow"):with_silent():with_noremap(),
-    --     ["n|<leader>mv"] = map_cr(":MarkdownPreview"):with_silent():with_noremap(),
-    --     ["n|<leader>R"] = map_cr(":Telescope registers"):with_silent():with_noremap(),
-    -- Trouble
-    --     ["n|<leader>xx"] = map_cr(":TroubleToggle"):with_silent():with_noremap(),
-    --     ["n|<leader>xw"] = map_cr(":TroubleToggle lsp_workspace_diagnostics"):with_silent():with_noremap(),
-    --     ["n|<leader>xd"] = map_cr(":TroubleToggle lsp_document_diagnostics"):with_silent():with_noremap(),
-    --     ["n|<leader>xq"] = map_cr(":TroubleToggle quickfix"):with_silent():with_noremap(),
-    --     ["n|<leader>xl"] = map_cr(":TroubleToggle loclist"):with_silent():with_noremap(),
-    --     ["n|<leader>gR"] = map_cr(":TroubleToggle lsp_references"):with_silent():with_noremap()
-    --     ["n|<leader>s"] = map_cr(":set spell"):with_silent():with_noremap()
 }
 
 bind.nvim_load_mapping(plug_map)
