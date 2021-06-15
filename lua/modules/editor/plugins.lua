8127f2cd1fdfcdc86cf2884ac7773d6a3ecbbcb7
rebuilt <memoryman51@hotmail.com>
2021-06-15 09:07:03 +0200

    add apathy

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -28,6 +28,10 @@ editor["tpope/vim-repeat"] = {
   event = {"InsertEnter"},
 }
 
+editor["tpope/vim-apathy"] = {
+  ft =  {"c", "cpp", "go", "javascript", "typescript", "javascriptreact", "typescriptreact", "coffeescript", "lua", "python", "scheme", "bash", "markdown"}
+}
+
 editor["kana/vim-niceblock"] = {
     opt = true
 }
@@ -58,4 +62,6 @@ editor["f-person/git-blame.nvim"] = {
 editor['christoomey/vim-sort-motion'] = {
   keys = "gs"
 }
+
+
 return editor
